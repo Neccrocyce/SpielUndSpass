2e0c0d0ce860df080e6640db12ebe325e551ff89
Bernie <bernie.gaida@yahoo.de>
2017-03-15 17:41:24 +0100

    ADDED Manager.renameTeam

--- src/content/Manager.java
+++ src/content/Manager.java
@@ -1,14 +1,29 @@
 package content;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manager {
-	private Poll[] polls;
+	private List<Poll> polls;
 	private Team[] teams;
 	private Game[] games;
 	
 	public Manager (Game[] games, int numOfPlayers) {
+		this.polls = new ArrayList<>();
 		this.games = games;
 		this.teams = new Team[2];
 		teams[0] = new Team("Team 1", (numOfPlayers % 2) == 0 ? numOfPlayers / 2 : numOfPlayers / 2 + 1);
 		teams[1] = new Team("Team 2", (numOfPlayers % 2) == 0 ? numOfPlayers / 2 : numOfPlayers / 2 + 1);
 	}
+	
+	/**
+	 * 
+	 * @param team number of Team - 0: Team 1; 1: Team 2
+	 * @param name
+	 */
+	public void renameTeam (int team, String name) {
+		teams[team].setName(name);
+	}
+	
+	
 }
